15cab245b9a7ad14627b2e04ffe518e212f90842
Andre <aluizdacosta@gmail.com>
2025-02-07 18:49:42 -0300

    create repo

---
+++ typescript/src/tutorial.ts
@@ -0,0 +1,17 @@
+console.log('typescript tutorial');
+
+let awesomeName: string = 'shakeAndBake';
+console.log(awesomeName.toUpperCase());
+
+let number: number = 10;
+console.log(number.toFixed(2));
+
+let isAwesome: boolean = true;
+console.log(!isAwesome);
+
+let nothing: void = undefined;
+console.log(nothing);
+
+let notDefined: undefined = undefined;
+console.log(notDefined);
+
