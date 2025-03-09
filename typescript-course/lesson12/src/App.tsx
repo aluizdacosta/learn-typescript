15ed299f533e2130ecb1e4bd5f0f33ced602a370
Andre <aluizdacosta@gmail.com>
2025-03-09 12:08:55 -0300

    lesson 12

---
+++ typescript-course/lesson12/src/App.tsx
@@ -0,0 +1,26 @@
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+import Counter from "./components/Counter"
+import { useState } from "react";
+import List from "./components/List";
+
+function App() {
+
+  const [count, setCount] = useState<number>(1)
+
+
+  return (
+    <>
+      <Heading title="Test Title"/>
+      <Section title="different title">This is my Section</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List items={["coffee", "Tacos", "Code"]} render={(item: string) => (
+        <span className="bold">{item}</span>
+      )} />
+    </>
+    
+  )
+  
+}
+
+export default App
